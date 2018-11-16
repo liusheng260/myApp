1c51a519c0b3dac4cd69b4847f42ee6a70ce8e7b
sheng.liu <sheng.liu@baisonmail.com>
2018-11-16 18:04:06 +0800

    文章新增

--- demo/admin.py
+++ demo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
-# from .models import Article
-# admin.site.register(Article)
+from .models import Article
+admin.site.register(Article)
